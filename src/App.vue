3a3a97aa103ac42cea9f085f2a09d48fbad95bcb
Lee Connelly <lee@leeconnelly.dev>
2023-07-18 21:39:09 +0100

    Create Sortable component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Sortable from '@/components/Sortable.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Sortable>
+    <div>Item 1</div>
+    <div>Item 2</div>
+    <div>Item 3</div>
+  </Sortable>
 </template>
 
 <style scoped>
---
+++ src/components/Sortable.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+</script>
+
+<template>
+  <div ref="list">
+    <slot />
+  </div>
+</template>
